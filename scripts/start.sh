efd2d830f45e5f9eeaf95b9ce984256289678d0e
Mario Cecchi <macecchi@gmail.com>
2018-04-01 22:35:50 -0300

    Add start script

---
+++ scripts/start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -eo pipefail
+
+SCRIPT_DIR=$( dirname "${BASH_SOURCE[0]}" )
+DOCKER_DIR="$SCRIPT_DIR/.."
+
+echo "Starting caronae-docker using the tag $CARONAE_ENV_TAG"
+/usr/local/bin/docker-compose -f "$DOCKER_DIR/docker-compose.yml" -f "$DOCKER_DIR/docker-compose.prod.yml" up -d
